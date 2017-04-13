9058c00a63de26d3f30d0ee53c3213100fb73ef7
eaglesant <eagle_21@mail.ru>
2017-04-13 13:15:26 +0300

    Add get_number_orders

--- public/app.js
+++ public/app.js
@@ -41,3 +41,19 @@ function get_number_of_itemsCart()
 	}
 	return cnt;
 }
+
+function get_number_orders()
+{
+	var orders = '';
+	for(var i=0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		var value = window.localStorage.getItem(key);
+		
+		if (key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+	return orders;
+}
