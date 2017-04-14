7b817175eabe9c28c281d55ac9fa2f7b982d9422
eaglesant <eagle_21@mail.ru>
2017-04-14 16:24:04 +0300

    Upgrade view cart

--- app.rb
+++ app.rb
@@ -23,21 +23,43 @@ end
 get '/pizzas' do
 	erb :pizzas
 end
-get '/cart' do
 
-	erb 'Hello'
-end
 post '/cart' do
-	@arr = []
-	@orders = params[:orders].split(',')
-	@orders.each do |i|
-		i.slice!(0..7)
-		@arr << i.split('=')
+	# @arr = []
+	# @orders = params[:orders].split(',')
+	# @orders.each do |i|
+	# 	i.slice!(0..7)
+	# 	@arr << i.split('=')
 		
+	# end
+	# @summ = 0
+	# @arr.each do |i|
+	# 	@summ += i[1].to_i
+	# end
+	# erb :cart
+
+	#=======================
+	orders = params[:orders]
+	@arr = parse_orders_item orders
+	@arr.each do |item|
+		item[0] = Product.find(item[0])
 	end
+	@qtt = 0
 	@summ = 0
-	@arr.each do |i|
-		@summ += i[1].to_i
-	end
 	erb :cart
+end
+def parse_orders_item orders_line
+	s1 = orders_line.split(',')
+arr = []
+	s1.each do |x|
+
+		s2 = x.split('=')
+		s3 = s2[0].split('_')
+
+		id = s3[1]
+		cnt = s2[1]
+		arr2 = [id,cnt]
+		arr.push arr2
+	end
+	return arr
 end
---
+++ app_test.rb
@@ -0,0 +1,17 @@
+orders_line = "product_1=2,product_2=3,product_3=4,"
+def parse_orders_item orders_line
+	s1 = orders_line.split(/,/)
+arr = []
+	s1.each do |x|
+
+		s2 = x.split(/=/)
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+		arr2 = [id,cnt]
+		arr.push arr2
+	end
+	return arr
+end
+puts parse_orders_item(orders_line)
